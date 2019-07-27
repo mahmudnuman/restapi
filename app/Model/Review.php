39543c7320fa1fc3c951304808fa52825ca94323
mahmudnuman <mahmudnuman@gmail.com>
2019-07-28 00:07:33 +0600

    One to Many Relationship Built

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,16 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 
+
+use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 class Review extends Model
 {
-    //
+   public function product(){
+
+
+   	return $this->belongsTo(Product::class);
+
+   }
 }
