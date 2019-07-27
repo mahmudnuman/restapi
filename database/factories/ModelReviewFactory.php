cc0ff70c64f0a58566eb801b4d63814c31f934e4
mahmudnuman <mahmudnuman@gmail.com>
2019-07-27 18:03:44 +0600

    fake data seeded

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+    	'product_id' =>function() {
+    	return Product::All()->Random();
+    	},
+
+    	'customer' => $faker->name,
+    	'star' => $faker->numberBetween(1,30),
+        'review' => $faker->paragraph,
+        
     ];
 });
